02f6b1c63d3eaefde9318e1fffca3731c2c8b53d
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-18 15:54:39 +0900

    Added an assertion on content

--- dfb/tests/test_root_endpoints.py
+++ dfb/tests/test_root_endpoints.py
@@ -3,7 +3,8 @@ from django.test import TestCase, Client
 
 class RootTestCase(TestCase):
 
-    def test_root_url_returns_200(self):
+    def test_root_url_returns_200_and_Helloworld(self):
         c = Client()
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello World")
