37d6742c77e1c44d0380125a74d2ee0acdb6ebc1
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-18 18:59:28 +0900

    Fixed error in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,14 +8,17 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 # Also import sys and site to help enable virtualenv
-import os, sys, site
+import os
+import sys
+import site
+from django.core.wsgi import get_wsgi_application
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.path.dirname(BASE_DIR)
 
 # Add the site packages, to override any system-wide packages
-site.addsitedir(os.path.join(REPO_DIR, '.env/lib/python3.5/site-packages'))
+site.addsitedir(os.path.join(BASE_DIR, '.env/lib/python3.5/site-packages'))
 
 # As is
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfb.settings")
@@ -23,8 +26,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfb.settings")
 # Calculate the path based on the location of the WSGI script
 project = BASE_DIR
 workspace = os.path.dirname(project)
-sys.path.append(workspace)
+sys.path.append(BASE_DIR)
 
-sys.path = [REPO_DIR, os.path.dirname(REPO_DIR)] + sys.path
-from django.core.wsgi import get_wsgi_application
+sys.path = [BASE_DIR, REPO_DIR, os.path.join(BASE_DIR, 'dfb')] + sys.path
 application = get_wsgi_application()
+
