4d2021a7f3d7a35fabce4818edef0d841ea1fb06
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-19 13:30:58 +0900

    First cut at deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Go to the project directory
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${PROJECT_DIR}
+
+# Setup virtualenv, if it doesn't already exist
+if [ -d ".env" ]; then
+    echo "**> virtualenv exists"
+else
+    echo "**> creating virtualenv"
+    virtualenv -p python3 .env
+    source .env/bin/activate
+    pip install -U pip-tools
+fi
+
+# Enter virtualenv
+set +u
+source .env/bin/activate
+set -u
+
+# Install dependencies
+pip-sync requirements.txt
+
+# Check for and apply new migrations
+python manage.py migrate --database default --no-input
+
+# Restart Apache
+../apche2/bin/restart
