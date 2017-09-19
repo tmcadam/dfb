98ec7e0d2b8e30d81ee0c13e5321f455520e3f43
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-19 17:55:36 +0900

    Added django-nose to ci environment

--- dfb/settings/__init__.py
+++ dfb/settings/__init__.py
@@ -4,12 +4,13 @@ from .base import BASE_DIR
 file_name = os.path.join(BASE_DIR, "ENVIRONMENT.cfg")
 with open(file_name) as f:
     ENVIRONMENT = f.readline()
+print(ENVIRONMENT)
 
 if ENVIRONMENT == 'staging':
     from .staging import *
 elif ENVIRONMENT == 'production':
     from .production import *
 elif ENVIRONMENT == 'ci':
-    from .production import *
+    from .ci import *
 elif ENVIRONMENT == 'development':
     from .development import *
