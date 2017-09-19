2c32c67da22eb5fec8d43c069948870aada2bf90
Tom McAdam <tmcadam@users.noreply.github.com>
2017-09-19 16:35:03 +0900

    Moved wsgi.py

--- dfb/wsgi.py
+++ dfb/wsgi.py
@@ -13,8 +13,8 @@ import sys
 import site
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-REPO_DIR = os.path.dirname(BASE_DIR)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+WEBSITE_DIR = os.path.dirname(BASE_DIR)
 
 # Add the site packages, to override any system-wide packages
 site.addsitedir(os.path.join(BASE_DIR, '.env/lib/python3.5/site-packages'))
@@ -23,11 +23,9 @@ site.addsitedir(os.path.join(BASE_DIR, '.env/lib/python3.5/site-packages'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfb.settings")
 
 # Calculate the path based on the location of the WSGI script
-project = BASE_DIR
-workspace = os.path.dirname(project)
 sys.path.append(BASE_DIR)
 
-sys.path = [BASE_DIR, REPO_DIR, os.path.join(BASE_DIR, 'dfb')] + sys.path
+sys.path = [BASE_DIR, WEBSITE_DIR, os.path.join(BASE_DIR, 'dfb')] + sys.path
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
